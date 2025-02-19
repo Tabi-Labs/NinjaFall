b025aa54a25a94b1cee0ae3816bbdcf69c08394a
Daniel Capilla Sánchez <capilloski@gmail.com>
2025-02-19 19:04:09 +0100

    PlayerNet

---
+++ Assets/_Scripts/Player/Player State Machine/StatesNet/PlayerWalkStateNet.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkStateNet : PlayerStateNet
+{
+    public PlayerWalkStateNet(PlayerNet player, PlayerStateMachineNet stateMachine) : base(player, stateMachine)
+    {
+    }
+
+    public override void StateEnter()
+    {
+        base.StateEnter();
+
+        _player.Anim.Play("p_Walk");
+        _player.Anim.SetBool(Player.IS_WALKING, true);
+    }
+
+    public override void StateExit()
+    {
+        base.StateExit();
+    }
+
+    public override void StateUpdate()
+    {
+        base.StateUpdate();
+
+        if (Mathf.Abs(_player.InputManager.Movement.x) > _moveStats.MoveThreshold && _player.InputManager.RunIsHeld)
+        {
+            _player.StateMachine.ChangeState(_player.RunState);
+        }
+
+        else if (Mathf.Abs(_player.InputManager.Movement.x) < _moveStats.MoveThreshold)
+        {
+            _player.Anim.SetBool(Player.IS_WALKING, false);
+
+            _player.StateMachine.ChangeState(_player.IdleState);
+        }
+
+        else if (_player.InputManager.JumpWasPressed)
+        {
+            if (_player.CanJump())
+            {
+                _player.SpawnJumpParticles(_player.JumpParticles);
+
+                _player.StateMachine.ChangeState(_player.JumpState);
+            }
+        }
+
+        else if (_player.JumpBufferedOrCoyoteTimed())
+        {
+            _player.SpawnJumpParticles(_player.JumpParticles);
+
+            _player.StateMachine.ChangeState(_player.JumpState);
+        }
+
+        if (_player.InputManager.DashWasPressed && (_player.CanDash() || _player.CanAirDash()))
+        {
+            _player.StateMachine.ChangeState(_player.DashState);
+        }
+    }
+
+    public override void StateFixedUpdate()
+    {
+        base.StateFixedUpdate();
+
+        //this gets called here for acceleration/movement
+        _player.Move(_moveStats.GroundAcceleration, _moveStats.GroundDeceleration, _player.InputManager.Movement);
+    }
+}
---
+++ Assets/_Scripts/Player/Player State Machine/StatesNet/PlayerWallJumpStateNet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallJumpStateNet : PlayerStateNet
+{
+    public PlayerWallJumpStateNet(PlayerNet player, PlayerStateMachineNet stateMachine) : base(player, stateMachine)
+    {
+    }
+
+    public override void StateEnter()
+    {
+        base.StateEnter();
+
+        _player.InitiateWallJump();
+        _player.StateMachine.ChangeState(_player.InAirState);
+    }
+
+    public override void StateExit()
+    {
+        base.StateExit();
+    }
+
+    public override void StateFixedUpdate()
+    {
+        base.StateFixedUpdate();
+
+        _player.WallJumpPhysics();
+
+        //movement
+
+        _player.Move(_moveStats.WallJumpMoveAcceleration, _moveStats.WallJumpMoveDeceleration, _player.InputManager.Movement);
+        
+    }
+
+    public override void StateUpdate()
+    {
+        base.StateUpdate();
+
+        if (_player.InputManager.DashWasPressed && (_player.CanDash() || _player.CanAirDash()))
+        {
+            _player.StateMachine.ChangeState(_player.DashState);
+        }
+
+
+    }
+}
