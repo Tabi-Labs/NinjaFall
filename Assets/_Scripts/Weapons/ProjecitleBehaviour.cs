62f17c931b50fc033ffa2b24e02348a615dd6c2f
Popeyes <menegom@icloud.com>
2025-03-03 22:37:21 +0100

    :sparkles: Feature: Shurikens get stuck on obstacles and can be picked up by player (It only destroys the shuriken for now)

--- Assets/_Scripts/Weapons/ProjecitleBehaviour.cs
+++ Assets/_Scripts/Weapons/ProjecitleBehaviour.cs
@@ -7,40 +7,41 @@ public class ProjectileBehaviour : MonoBehaviour
     [SerializeField] private ProjectileStats _stats;
     private Movement _movement;
     private Vector2 _direction;
+    private Animator _animator;
+    private bool _isMoving = true;
 
     #region ---- UNITY CALLBACKS ----
     private void Awake()
     {
         _movement = GetComponent<Movement>();
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+        if(!_isMoving) return;
         _movement.Move(_stats.MoveSpeed, _stats.AirAcceleration, _direction);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var damageableComponent = collision.GetComponentInParent<IDamageable>();
-        if(damageableComponent != null)
+        if(_isMoving)
         {
-            damageableComponent.TakeDamage(_stats.Damage);
-        
-      
+            CheckForDamageHit(collision.transform);
+            OnObstacleHit(collision.ClosestPoint(transform.position));
         }
-
-        Destroy(gameObject);
+        else 
+        {
+            CheckForPlayerPickup(collision.transform);
+        }
+       
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /* private void OnCollisionEnter2D(Collision2D collision)
     {
-        var damageableComponent = collision.transform.GetComponentInParent<IDamageable>();
-         if(damageableComponent != null)
-        {
-            damageableComponent.TakeDamage(_stats.Damage);
-            Destroy(gameObject);
-        }
-    }
+        CheckForDamageHit(collision.transform);
+        OnObstacleHit(collision.GetContact(0).point);
+    }    */
     #endregion
 
     #region ---- PROJECTILE BEHAVIOUR ----
@@ -48,6 +49,40 @@ public class ProjectileBehaviour : MonoBehaviour
     public void Init(Vector2 direction)
     {
         _direction = direction;
+    }
+
+    private void OnObstacleHit(Vector3 hitPoint)
+    {
+        _isMoving = false;
+        _movement.Stop();
+        transform.position = hitPoint;
+        _animator.enabled = false;  
+    }
+
+    private void CheckForDamageHit(Transform collider)
+    {
+       
+        var damageableComponent = collider.GetComponentInParent<IDamageable>();
+        if(damageableComponent != null)
+        {
+            damageableComponent.TakeDamage(_stats.Damage);
+            Destroy(gameObject);
+      
+        }
+    }
+
+    private void CheckForPlayerPickup(Transform collider)
+    {
+        /* var playerPickupComponent = collider.GetComponentInParent<IPlayerPickup>();
+        if(playerPickupComponent != null)
+        {
+            playerPickupComponent.Pickup();
+            Destroy(gameObject);
+        } */
+        if(collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
     #endregion
 }
