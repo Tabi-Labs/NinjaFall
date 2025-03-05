7c6342114d68ac8f0a40cd9d72d7efa1b6ac60e5
Juan Bautista Aballay <67266651+bautixter@users.noreply.github.com>
2025-03-05 19:33:31 +0100

    ✨ Transitions on change scene

--- Assets/_Scripts/Misc/SceneLoader.cs
+++ Assets/_Scripts/Misc/SceneLoader.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField]
+    Image transitionCourtain;
+    [SerializeField]
+    float transitionDuration = 0.5f;
+
     // Method to change scene by name
     public void ChangeScene(string sceneName)
     {
+        StartCoroutine(LoadSceneWithTransition(sceneName));
+    }
+
+    private IEnumerator LoadSceneWithTransition(string sceneName)
+    {
+        if (transitionCourtain != null)
+        {
+            yield return transitionCourtain.DOFade(1, transitionDuration).WaitForCompletion();
+        }
+
         SceneManager.LoadScene(sceneName);
+
+        if (transitionCourtain != null)
+        {
+            yield return transitionCourtain.DOFade(0, transitionDuration).WaitForCompletion();
+        }
     }
 }
