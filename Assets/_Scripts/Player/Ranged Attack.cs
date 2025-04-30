0f2098a424d55d21129dea4add8aa1e91894d394
Daniel Capilla Sánchez <capilloski@gmail.com>
2025-04-30 13:58:32 +0200

    ✨ Shuriken spawn (falta la animacion de apuntar)

--- Assets/_Scripts/Player/Ranged Attack.cs
+++ Assets/_Scripts/Player/Ranged Attack.cs
@@ -14,11 +14,10 @@ public class RangedAttack : NetworkBehaviour
     [SerializeField] GameObject _projectilePrefab;
     [SerializeField] Transform _projectileSpawnPoint;
 
-
     private bool wallBuff = false;
     private float gravityDebuff = 1.0f;
     private float gravityIgnoreTimer = 0.4f;
-    public int ShurikensCount{get;private set;}
+    public int ShurikensCount { get; private set; }
     public GameEvent shurikenAmmoEvent;
     private Color _debugColor = Color.red;
 
@@ -39,10 +38,10 @@ public class RangedAttack : NetworkBehaviour
 
     void Update()
     {
-        if(_attackStats.DebugAttackArea)
+        if (_attackStats.DebugAttackArea)
             DebugAttack(_debugColor);
 
-        if(_player.Input().IsAiming)
+        if (_player.Input().IsAiming)
         {
             AimShuriken();
         }
@@ -52,37 +51,50 @@ public class RangedAttack : NetworkBehaviour
 
     #region  ---- ATTACKS ------
 
-    void OnAim(int layerIndex)
-    {
-        
-    }
-
     void AimShuriken()
     {
         aimDirection = _player.Input().AimMovement;
 
-        if(aimDirection.SqrMagnitude() > 3f)
+        if (aimDirection.SqrMagnitude() > 3f)
         {
             aimDirection = Camera.main.ScreenToWorldPoint(aimDirection) - _projectileSpawnPoint.position;
             aimDirection.Normalize();
         }
     }
+
     void OnRangedAttack()
     {
-        if (NetworkManager)
+        // Validación común para todos los modos
+        if (_player.IsWallSliding && _player.IsWallSlideFalling) return;
+        if (ShurikensCount >= _attackStats.MaxShurikens)
         {
-            if (!IsOwner) return;
-            RequestSpawnProjectileRPC();
+            NoAmmoFX();
             return;
         }
-     
-        RequestShuriken(aimDirection);
+
+        // Usar la dirección de apuntado o la dirección por defecto
+        Vector3 direction = aimDirection == default ? transform.right : aimDirection;
+
+        if (NetworkManager && NetworkManager.IsListening)
+        {
+            // Estamos en modo red
+            if (!IsOwner) return; // Solo el dueño puede disparar
+
+            // Solicitar al servidor que instancie el proyectil
+            SpawnProjectileServerRpc(direction);
+        }
+        else
+        {
+            // Estamos en modo local
+            SpawnProjectileLocally(direction);
+        }
+
+        // Resetear la dirección de apuntado
+        aimDirection = default;
     }
 
     public void ApplyGravity(float newGravity)
     {
-        Debug.Log("Clase Ranged Attack");
-
         gravityDebuff = newGravity;
 
         if (newGravity > 1.0f)
@@ -93,91 +105,140 @@ public class RangedAttack : NetworkBehaviour
         {
             gravityIgnoreTimer = 0.4f;
         }
-
     }
 
     public void ApplyWallBuff(bool buff)
     {
-        Debug.Log("Shuriken wall buff - Se intenta aplicar: " + buff);
         wallBuff = buff;
-
     }
 
-    [Rpc(SendTo.Server)]
-    void RequestSpawnProjectileRPC()
+    [ServerRpc]
+    void SpawnProjectileServerRpc(Vector3 direction)
     {
-        if (_player.IsWallSliding && _player.IsWallSlideFalling) return;
-        if (ShurikensCount >= _attackStats.MaxShurikens)
-        {
-            NoAmmoFX();
-            return;
-        }
-
-        Vector3 direction = aimDirection == default ? transform.right : aimDirection;
+        // Instanciar el proyectil en el servidor
         var projectile = Instantiate(_projectilePrefab, _projectileSpawnPoint.position, Quaternion.identity);
         projectile.GetComponent<ProjectileBehaviour>().Init(direction, _selfDamageable, true, wallBuff, gravityIgnoreTimer, gravityDebuff);
-        NetworkObject networkObject = projectile.GetComponent<NetworkObject>();
-        networkObject.Spawn();
 
+        // Hacer spawn del objeto en la red
+        NetworkObject networkObject = projectile.GetComponent<NetworkObject>();
+        if (networkObject != null)
+        {
+            networkObject.Spawn();
+        }
+
+        // Actualizar el contador de shurikens en el cliente que disparó
+        UpdateShurikenCountClientRpc(NetworkManager.LocalClientId);
+    }
+
+    [ClientRpc]
+    void UpdateShurikenCountClientRpc(ulong clientId)
+    {
+        if (IsOwner && OwnerClientId == clientId)
+        {
+            ShurikensCount++;
+            if (ShurikensCount > _attackStats.MaxShurikens) ShurikensCount = _attackStats.MaxShurikens;
+
+            if (shurikenAmmoEvent != null)
+            {
+                shurikenAmmoEvent.Raise(_player, ShurikensCount);
+            }
+
+            ShurikenFX();
+        }
+    }
+
+    void SpawnProjectileLocally(Vector3 direction)
+    {
+        // Instanciar el proyectil localmente (sin red)
+        var projectile = Instantiate(_projectilePrefab, _projectileSpawnPoint.position, Quaternion.identity);
+        projectile.GetComponent<ProjectileBehaviour>().Init(direction, _selfDamageable, true, wallBuff, gravityIgnoreTimer, gravityDebuff);
+
+        // Actualizar contador local
         ShurikensCount++;
         if (ShurikensCount > _attackStats.MaxShurikens) ShurikensCount = _attackStats.MaxShurikens;
 
-        shurikenAmmoEvent.Raise(_player, ShurikensCount);
-
-        ShurikenFX();
-
-        aimDirection = default;
-    }
-
-    void RequestShuriken(Vector3 direction = default)
-    {
-        if (_player.IsWallSliding && _player.IsWallSlideFalling) return;
-        if (ShurikensCount >= _attackStats.MaxShurikens) 
+        if (shurikenAmmoEvent != null)
         {
-            NoAmmoFX();
-            return;
+            shurikenAmmoEvent.Raise(_player, ShurikensCount);
         }
 
-        if (direction == default) direction = transform.right;
-        var projectile = Instantiate(_projectilePrefab, _projectileSpawnPoint.position, Quaternion.identity);
-        projectile.GetComponent<ProjectileBehaviour>().Init(direction, _selfDamageable, true, wallBuff, gravityIgnoreTimer,gravityDebuff);
-        aimDirection = default;
-
-        ShurikensCount++;
-        if(ShurikensCount > _attackStats.MaxShurikens) ShurikensCount = _attackStats.MaxShurikens;
-        
-        shurikenAmmoEvent.Raise(_player, ShurikensCount);
-        
         ShurikenFX();
-       
     }
+
     void DebugAttack(Color color)
     {
         Debug.DrawRay(_projectileSpawnPoint.position, transform.right * _attackStats.RangedAttackRange, color);
     }
 
     public void OnShurikenPickedUp()
-    {   
+    {
         ShurikensCount--;
         if (ShurikensCount < 0) ShurikensCount = 0;
-    
-        shurikenAmmoEvent.Raise(_player, ShurikensCount);
+
+        if (shurikenAmmoEvent != null)
+        {
+            shurikenAmmoEvent.Raise(_player, ShurikensCount);
+        }
     }
 
+    // Destroy the shuriken if it is PICKED UP
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Shuriken"))
+        if (collision.CompareTag("Shuriken"))
         {
             var shuriken = collision.GetComponent<ProjectileBehaviour>();
-            if(shuriken.IsMoving) return;
-            Destroy(shuriken.gameObject);
+            if (shuriken.IsMoving) return;
+
+            // En modo red, solo el servidor puede destruir objetos
+            if (NetworkManager && NetworkManager.IsListening)
+            {
+                if (IsServer)
+                {
+                    Destroy(shuriken.gameObject);
+                    OnShurikenPickupServerRpc(OwnerClientId);
+                }
+                else if (IsOwner)
+                {
+                    RequestPickupShurikenServerRpc(shuriken.NetworkObject);
+                }
+            }
+            else
+            {
+                // En modo local, destruimos directamente
+                Destroy(shuriken.gameObject);
+                OnShurikenPickedUp();
+            }
+        }
+    }
+
+    [ServerRpc]
+    void RequestPickupShurikenServerRpc(NetworkObjectReference shurikenRef)
+    {
+        if (shurikenRef.TryGet(out NetworkObject shurikenObject))
+        {
+            Destroy(shurikenObject.gameObject);
+            OnShurikenPickupServerRpc(OwnerClientId);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void OnShurikenPickupServerRpc(ulong clientId)
+    {
+        OnShurikenPickupClientRpc(clientId);
+    }
+
+    [ClientRpc]
+    void OnShurikenPickupClientRpc(ulong clientId)
+    {
+        if (IsOwner && OwnerClientId == clientId)
+        {
             OnShurikenPickedUp();
         }
     }
     #endregion
 
     #region ------- FX -------
-    
+
     void NoAmmoFX()
     {
         AudioManager.PlaySound("FX_NoShurikens");
--- Assets/_Scripts/Weapons/ProjectileBehaviour.cs
+++ Assets/_Scripts/Weapons/ProjectileBehaviour.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ProjectileBehaviour : MonoBehaviour
+public class ProjectileBehaviour : NetworkBehaviour
 {
     [SerializeField] private ProjectileStats _stats;
     private Movement _movement;
@@ -26,6 +27,7 @@ public class ProjectileBehaviour : MonoBehaviour
     private float timerFollowingEdge = 5.0f;
 
     public bool IsMoving => _isMoving;
+
     #region ---- UNITY CALLBACKS ----
     private void Awake()
     {
@@ -38,94 +40,187 @@ public class ProjectileBehaviour : MonoBehaviour
 
     private void Update()
     {
-        OwnerInvulnerabilityTimer();   
+        
+        OwnerInvulnerabilityTimer();
         GravityIgnoreTimer();
     }
+
     private void FixedUpdate()
     {
-
-        if (isFollowingEdge && currentObstacle != null)
+        // En modo red, solo el servidor controla el movimiento
+        // El ClientNetworkTransform se encargará de sincronizar la posición
+        if (!NetworkManager || IsServer)
         {
-            Debug.Log("Obstaculo: " + currentObstacle.name);
-            FollowEdge();
-
-            if (timerFollowingEdge <= 0.0f)
+            if (isFollowingEdge && currentObstacle != null)
             {
-                StopShuriken();
+                FollowEdge();
+
+                if (timerFollowingEdge <= 0.0f)
+                {
+                    StopShuriken();
+                }
+            }
+            else
+            {
+                if (!_isMoving) return;
+                _movement.Move(_stats.MoveSpeed, _stats.AirAcceleration, _direction);
+                if (_isAffectedByGravity)
+                    _movement.ApplyGravity(_stats.Gravity * _gravityDebuff, _stats.MaxFallSpeed);
+                _movement.VerticalMove(_stats.MoveSpeed, _stats.AirAcceleration, _direction);
             }
         }
-        else
-        {
-            if (!_isMoving) return;
-            _movement.Move(_stats.MoveSpeed, _stats.AirAcceleration, _direction);
-            if (_isAffectedByGravity)
-                _movement.ApplyGravity(_stats.Gravity * _gravityDebuff, _stats.MaxFallSpeed);
-            _movement.VerticalMove(_stats.MoveSpeed, _stats.AirAcceleration, _direction);
-        }
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(_isMoving)
+        // En modo red, solo el servidor procesa colisiones para evitar duplicados
+        if (!NetworkManager || IsServer)
         {
-            var damageableComponent = collision.GetComponentInParent<IDamageable>();
+            if (_isMoving)
+            {
+                var damageableComponent = collision.GetComponentInParent<IDamageable>();
 
-            if( collision.CompareTag("Shuriken"))
-            {
-                _direction = new Vector2(0.0f, -1.0f);
-            } 
-            else if (!collision.isTrigger)
-            {
-                CheckForDamageHit(damageableComponent);
-                if (damageableComponent != null && damageableComponent == _owner) return;
-                OnObstacleHit(collision.ClosestPoint(transform.position), collision);
+                if (collision.CompareTag("Shuriken"))
+                {
+                    _direction = new Vector2(0.0f, -1.0f);
+
+                    // Notificar a los clientes del cambio de dirección
+                    if (NetworkManager && IsServer)
+                    {
+                        ReflectShurikenClientRpc(_direction);
+                    }
+                }
+                else if (!collision.isTrigger)
+                {
+                    CheckForDamageHit(damageableComponent);
+                    if (damageableComponent != null && damageableComponent == _owner) return;
+                    OnObstacleHit(collision.ClosestPoint(transform.position), collision);
+                }
+                else if (damageableComponent != null)
+                    AutoAim(collision.transform, damageableComponent);
             }
-            else if (damageableComponent != null)
-                AutoAim(collision.transform, damageableComponent);
-
         }
-
-       
     }
-
     #endregion
 
     #region ---- PROJECTILE BEHAVIOUR ----
-
-    public void Init(Vector2 direction, IDamageable owner, bool canDamageOwner, bool wallBuff, float _gravityTimer, float gravity)
+    // Método para inicializar el proyectil cuando se crea
+    public void Init(Vector2 direction, IDamageable owner, bool canDamageOwner, bool wallBuff, float gravityTimer, float gravity)
     {
         _direction = direction;
         _owner = owner;
         _canDamageOwner = canDamageOwner;
         _shurikenWallBuff = wallBuff;
-        _gravityIgnoreTimer = _gravityTimer;
+        _gravityIgnoreTimer = gravityTimer;
         _gravityDebuff = gravity;
+
+        // Si estamos en red y somos el servidor, sincronizar con los clientes
+        if (NetworkManager && IsServer)
+        {
+            InitClientRpc(direction, wallBuff, gravityTimer, gravity);
+        }
     }
 
+    [ClientRpc]
+    private void InitClientRpc(Vector2 direction, bool wallBuff, float gravityTimer, float gravity)
+    {
+        // No actualizamos en el servidor, ya que ya tiene los valores correctos
+        if (!IsServer)
+        {
+            _direction = direction;
+            _shurikenWallBuff = wallBuff;
+            _gravityIgnoreTimer = gravityTimer;
+            _gravityDebuff = gravity;
+        }
+    }
+
+    // Método para reflejar el shuriken cuando rebota
     public void ReflectShuriken(Vector2 newDirection)
     {
-        if(newDirection == Vector2.zero)
+        if (newDirection == Vector2.zero)
         {
             newDirection.x = -_direction.x;
             newDirection.y = 0.0f;
         }
 
-        Debug.Log("Nueva direccion shuriken" + newDirection);
-        _direction = newDirection;
+        // Solo el servidor cambia la dirección en red
+        if (!NetworkManager || IsServer)
+        {
+            _direction = newDirection;
+
+            // Si estamos en red y somos el servidor, sincronizar con los clientes
+            if (NetworkManager)
+            {
+                ReflectShurikenClientRpc(newDirection);
+            }
+        }
+        else if (NetworkManager)
+        {
+            // Si somos cliente, solicitamos al servidor cambiar la dirección
+            ReflectShurikenServerRpc(newDirection);
+        }
     }
 
+    [ServerRpc(RequireOwnership = false)]
+    private void ReflectShurikenServerRpc(Vector2 newDirection)
+    {
+        _direction = newDirection;
+
+        // Propagar el cambio a todos los clientes
+        ReflectShurikenClientRpc(newDirection);
+    }
+
+    [ClientRpc]
+    private void ReflectShurikenClientRpc(Vector2 newDirection)
+    {
+        // No actualizamos en el servidor, ya que ya tiene la dirección correcta
+        if (!IsServer)
+        {
+            _direction = newDirection;
+        }
+    }
 
     private void OnObstacleHit(Vector3 hitPoint, Collider2D collision)
     {
-        Debug.Log("Shuriken wall buff on hit: " + _shurikenWallBuff);
         AudioManager.PlaySound("FX_ShurikenHit");
         if (_shurikenWallBuff)
         {
             isFollowingEdge = true;
             StartFollowingEdge(collision);
 
-        } else
+            // Sincronizar con los clientes que el shuriken está siguiendo un borde
+            if (NetworkManager && IsServer)
+            {
+                StartFollowingEdgeClientRpc(hitPoint);
+            }
+        }
+        else
+        {
+            transform.position = hitPoint;
+            StopShuriken();
+
+            // Sincronizar con los clientes que el shuriken se ha detenido
+            if (NetworkManager && IsServer)
+            {
+                StopShurikenClientRpc(hitPoint);
+            }
+        }
+    }
+
+    [ClientRpc]
+    private void StartFollowingEdgeClientRpc(Vector3 hitPoint)
+    {
+        if (!IsServer)
+        {
+            // Marcar que está siguiendo el borde, pero el movimiento lo controla el servidor
+            isFollowingEdge = true;
+        }
+    }
+
+    [ClientRpc]
+    private void StopShurikenClientRpc(Vector3 hitPoint)
+    {
+        if (!IsServer)
         {
             transform.position = hitPoint;
             StopShuriken();
@@ -134,22 +229,75 @@ public class ProjectileBehaviour : MonoBehaviour
 
     private void CheckForDamageHit(IDamageable damageableComponent)
     {
-        if(damageableComponent != null)
+        if (damageableComponent != null)
         {
-            if(_owner != null && damageableComponent == _owner && !_shouldDamageOwner) return;
+            if (_owner != null && damageableComponent == _owner && !_shouldDamageOwner) return;
             damageableComponent.TakeDamage(_stats.Damage);
-            Destroy(gameObject);
-      
+            DestroyProjectile();
         }
     }
 
+    private void DestroyProjectile()
+    {
+        if (NetworkManager)
+        {
+            if (IsServer)
+            {
+                // Si somos servidor, despachamos directamente
+                if (NetworkObject != null && NetworkObject.IsSpawned)
+                {
+                    NetworkObject.Despawn();
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                // Si somos cliente, solicitamos al servidor que despache
+                RequestDespawnServerRpc();
+            }
+        }
+        else
+        {
+            // Si no estamos en red, destruimos localmente
+            Destroy(gameObject);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestDespawnServerRpc()
+    {
+        if (NetworkObject != null && NetworkObject.IsSpawned)
+        {
+            NetworkObject.Despawn();
+        }
+    }
 
     private void AutoAim(Transform target, IDamageable damageableComponent)
     {
-        if(damageableComponent != null && damageableComponent == _owner) return;
+        if (damageableComponent != null && damageableComponent == _owner) return;
         var directionToTarget = (target.position - transform.position).normalized;
-        if(Vector2.Dot(_direction, directionToTarget) > 0)
+        if (Vector2.Dot(_direction, directionToTarget) > 0)
+        {
             _direction = Vector2.Lerp(_direction, directionToTarget, _stats.RedirectionAcceleration * Time.deltaTime);
+
+            // Sincronizar la nueva dirección después del auto-aim
+            if (NetworkManager && IsServer)
+            {
+                AutoAimClientRpc(_direction);
+            }
+        }
+    }
+
+    [ClientRpc]
+    private void AutoAimClientRpc(Vector2 newDirection)
+    {
+        if (!IsServer)
+        {
+            _direction = newDirection;
+        }
     }
 
     private void StartFollowingEdge(Collider2D obstacle)
@@ -157,36 +305,34 @@ public class ProjectileBehaviour : MonoBehaviour
         currentObstacle = obstacle;
         isFollowingEdge = true;
 
-        // Calcular direcci�n tangencial inicial
+        // Calcular dirección tangencial inicial
         Vector2 closestPoint = obstacle.ClosestPoint(transform.position);
         Vector2 surfaceNormal = ((Vector2)transform.position - closestPoint).normalized;
         tangentDirection = Vector2.Perpendicular(surfaceNormal);
 
-        // Determinar direcci�n basada en la velocidad inicial
+        // Determinar dirección basada en la velocidad inicial
         float direction = Vector2.Dot(_direction.normalized, tangentDirection) > 0 ? 1 : -1;
         tangentDirection *= direction;
-
     }
 
     private void FollowEdge()
     {
-        // Obtener punto m�s cercano en el obst�culo
+        // Obtener punto más cercano en el obstáculo
         Vector2 closestPoint = currentObstacle.ClosestPoint(transform.position);
 
         // Calcular nueva normal y tangente
         Vector2 surfaceNormal = ((Vector2)transform.position - closestPoint).normalized;
         Vector2 desiredTangent = Vector2.Perpendicular(surfaceNormal);
 
-        // Mantener la direcci�n original de rotaci�n
+        // Mantener la dirección original de rotación
         float rotationDirection = Vector2.Dot(tangentDirection, desiredTangent) > 0 ? 1 : -1;
         desiredTangent *= rotationDirection;
 
-        // Aplicar fuerza centr�peta
+        // Aplicar fuerza centrípeta
         Vector2 steerForce = desiredTangent * _stats.MoveSpeed - _direction;
         _movement.Move(_stats.MoveSpeed, _stats.AirAcceleration, steerForce);
 
         timerFollowingEdge -= Time.deltaTime;
-
     }
 
     private void StopShuriken()
@@ -196,30 +342,27 @@ public class ProjectileBehaviour : MonoBehaviour
         _movement.Stop();
         _animator.enabled = false;
     }
-
     #endregion
 
     #region ---- TIMERS ----
-
     private void OwnerInvulnerabilityTimer()
     {
-        if(!_runInvulnerabilityTimer) return;
-        if(!_canDamageOwner) return;
+        if (!_runInvulnerabilityTimer) return;
+        if (!_canDamageOwner) return;
 
         _invulnerabilityTimer -= Time.deltaTime;
 
-        if(_invulnerabilityTimer <= 0)
+        if (_invulnerabilityTimer <= 0)
         {
             _runInvulnerabilityTimer = false;
             _shouldDamageOwner = true;
         }
-            
     }
 
     private void GravityIgnoreTimer()
     {
         _gravityIgnoreTimer -= Time.deltaTime;
-        if(_gravityIgnoreTimer <= 0)
+        if (_gravityIgnoreTimer <= 0)
         {
             _isAffectedByGravity = true;
         }
